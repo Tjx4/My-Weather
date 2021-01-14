283a3c9cc6aa8d3c3ce61784b40e12a991ff0093
Tshepo <rocboyt@gmail.com>
2021-01-14 09:33:39 +0200

    Added weather activity

---
+++ app/src/main/java/com/tykaway/myweather/features/WeatherActivity.kt
@@ -0,0 +1,12 @@
+package com.tykaway.myweather.features
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tykaway.myweather.R
+
+class WeatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+    }
+}

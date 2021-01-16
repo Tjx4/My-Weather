b3fdda7aa178f38f17a9e0c5706952bc0f06d9b7
Tshepo <rocboyt@gmail.com>
2021-01-16 14:32:39 +0200

    updated converter

--- app/src/main/java/com/globalkinetic/myweather/converter/tempratureConverter.kt
+++ app/src/main/java/com/globalkinetic/myweather/converter/tempratureConverter.kt
@@ -3,7 +3,7 @@ package com.globalkinetic.myweather.converter
 fun fahrenheitToCelsius(fahrenheit: Double): Int{
     return  try {
         var celsius = (fahrenheit - 32.0) * 5.0 / 9.0
-        celsius /= 5
+        celsius /= 5.2
         celsius.toInt()
     }
     catch (ex: Exception){

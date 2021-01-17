d962cba9498f6346c6f558e338898be2e6f2e0ba
Tshepo <rocboyt@gmail.com>
2021-01-17 12:47:34 +0200

    Changed weather units to metric

---
+++ app/src/main/java/com/globalkinetic/myweather/networking/RetrofitHelper.kt
@@ -0,0 +1,11 @@
+package com.globalkinetic.myweather.networking
+
+import com.globalkinetic.myweather.models.Weather
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface RetrofitHelper {
+    @GET("data/2.5/onecall")
+    suspend fun getMyLocationWeather(@Query("appid") appid: String, @Query("lat") lat: Double, @Query("lon") lon: Double, @Query("units") units: String): Weather?
+}

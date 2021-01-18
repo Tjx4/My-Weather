d3eb7e514b825c9e6ee72a4d13cfc4bf5a7800df
Tshepo <rocboyt@gmail.com>
2021-01-18 09:35:40 +0200

    Pre theme fix

--- app/src/main/java/com/globalkinetic/myweather/helpers/IncontextPermissionHelper.kt
+++ app/src/main/java/com/globalkinetic/myweather/helpers/IncontextPermissionHelper.kt
@@ -3,18 +3,22 @@ package com.globalkinetic.myweather.helpers
 import android.Manifest
 import android.app.Activity
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.core.app.ActivityCompat
 
-
 fun checkLocationPermissionAndContinue(context: Activity, onSuccessCallback: () -> Unit = {}) {
-    if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-        onSuccessCallback.invoke()
+     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            onSuccessCallback.invoke()
+        } else {
+            ActivityCompat.requestPermissions(
+                    context,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    1
+            )
+        }
     } else {
-        ActivityCompat.requestPermissions(
-            context,
-            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-            1
-        )
+         onSuccessCallback.invoke()
     }
 }
 
